c25665f0fd0876d2e070b58c99bbac01f875c8d2
onflyer <salkaorama@gmail.com>
2024-05-04 15:29:41 +0200

    Initial Commit

---
+++ HomeTask/HomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTaskApp.swift
+//  HomeTask
+//
+//  Created by Aleksandar Milidrag on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
